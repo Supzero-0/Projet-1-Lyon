ac3c9931a8767f403dd215112a8fbf5332194ce6
CoquilledeLune <ljacobieski@gmail.com>
2022-09-28 17:21:37 +0200

    Modifications de la nav barre

---
+++ _mainNav.php
@@ -0,0 +1,17 @@
+<div class="navBar">
+   <div>
+    Accueil
+  </div>
+  <div>
+   Vieux Lyon
+  </div>
+  <div>
+   Croix Rousse
+   </div>
+   <div>
+  Guillotière
+  </div>
+  <div>
+  Lyon 9
+  </div>
+</div>
