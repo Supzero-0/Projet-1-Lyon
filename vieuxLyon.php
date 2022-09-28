08cd6b70b14efb3e180c6b52fd9266868f4d97e9
Matthieu Dufloux <matthieu.dufloux.@pro@gmail.com>
2022-09-28 17:19:42 +0200

    dossier img et backgroundimg

---
+++ vieuxLyon.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<?php include("_mainNav.php") ?>
+
+<body>
+
+    <h1>Le Vieux Lyon</h1>
+
+
+    <?php include("_footer.php") ?>
+</body>
+
+
+</html>
