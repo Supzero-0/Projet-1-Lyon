7975b89ace9d0cd6efe0f37f221bc32fc92e61f3
CoquilledeLune <ljacobieski@gmail.com>
2022-09-29 15:07:49 +0200

    Quelques points virgules et le body n'est pas bon

---
+++ lyon9.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+</head>
+<body>
+  <main>
+    <h1>
+      Lyon 9
+    </h1>
+    <h2>
+      Histoire
+    </h2>
+      <p>
+      Le 9e arrondissement de Lyon est l'un des neuf arrondissements de la ville française de Lyon.
+      Il comprend les quartiers de Vaise, Gorge de Loup, Saint-Rambert-l'Île-Barbe, La Duchère, Champvert-Nord et Montribloud. Il s'étale sur la rive droite de la Saône, ainsi que sur l'île Barbe, seule île fluviale lyonnaise. Il constitue l'arrondissement le plus septentrional de Lyon. 
+      Le 9e arrondissement de Lyon est créé par le détachement de la partie septentrionale du 5e arrondissement (Décret du 12 août 1964), comprenant notamment l'ancienne commune de Saint-Rambert-l'Île-Barbe, annexée l'année précédente (décret du 1er août 1963).   
+      </p>
+      <img src="Images/pexels-atypeek-dgn-6036842.jpg" alt="Plan de Lyon 9" class="lyonMap">
+    <h2>
+      Culture
+    </h2>
+    <p>
+      Bla bla bla
+    </p>
+    <h2>
+      Restauration
+    </h2>
+    <p>
+      Bla bla bla
+    </p>
+    <h2>
+      Activités
+    </h2>
+    <p>
+      Bla bla bla
+    </p>
+  </main>
+</body>
+</html>
