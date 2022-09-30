ee43174171e4a750d676508b149b64f47a0ae4cb
Matthieu Dufloux <matthieu.dufloux.@pro@gmail.com>
2022-09-30 22:01:23 +0200

    map interactive

--- index.php
+++ index.php
@@ -18,7 +18,12 @@
     <header>
         <h1>Bienvenue à Lyon</h1>
     </header>
-
+    <div>
+        <img src="Images/Capture d’écran 2022-09-28 à 15.30.19.png" alt="lyonMap" usemap='#carteLyon'>
+        <map name='carteLyon'>
+            <area class="lyon9" shape="poly" coords="742,104,465,361,425,471,444,741,224,667,184,308,345,273,642,68" href="lyon9.php" target="_blank" alt="lyonNeuf">
+        </map>"
+    </div>
     <main>
         <h2>Qui sommes-nous ?</h2>
         <p>Bla bla bla</p>
