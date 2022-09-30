afea87ae2ca41f45063c870410f8f112039bc1cf
HUGO TAPIA <h.hugotapia@gmail.com>
2022-09-30 17:27:52 +0200

    modified footer

--- _footer.php
+++ _footer.php
@@ -1,20 +1,17 @@
 <footer>
     <navbar>
         <ul>
-
-
             <a class="logo-link" href="https://www.lyon-france.com/" target="_blank">
-                <img src="" alt="Only Lyon logo"></a>
+                <img src="Images/Logos footer/Only Lyon logo.png" alt="Only Lyon logo"></a>
 
             <a class="logo-link" href="https://www.lyon.fr/" target="_blank">
-                <img src="https://www.lyon.fr/themes/custom/lyonfr/images/LOGO-ville_de_lyon.svg" alt="Ville Lyon logo"></a>
+                <img src="Images\Logos footer\VDL-logo.jpg" alt="Ville Lyon logo"></a>
 
             <a class="logo-link" href="https://www.tcl.fr/" target="_blank">
-                <img src="https://www.tcl.fr/themes/custom/sytral_theme/img/logo/tcl-sytral.svg" alt="TCL logo"></a>
+                <img src="Images\Logos footer\logo TCL.png" alt="TCL logo"></a>
 
             <a class="logo-link" href="https://www.routard.com/guide_a_voir/lyon.htm" target="_blank">
-                <img src="https://media.routard.com/design/frontend/images/v2/common/logo.png" alt="Routard logo"></a>
-
+                <img src="Images\Logos footer\logo routard.png" alt="Routard logo"></a>
         </ul>
     </navbar>
 
