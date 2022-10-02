d22777962c8a0fbaec4340544dea443e8ff76e38
CoquilledeLune <ljacobieski@gmail.com>
2022-10-02 20:25:35 +0200

    debut de highlight de carte

--- index.php
+++ index.php
@@ -14,23 +14,25 @@
 
     <?php include("_mainNav.php"); ?>
 
-    <script src="_mainNav.js"></script>
     <header>
         <h1>Bienvenue à Lyon</h1>
     </header>
-    <div>
-        <img src="Images/Capture d’écran 2022-09-28 à 15.30.19.png" alt="lyonMap" usemap='#carteLyon'>
-        <map name='carteLyon'>
-            <area class="lyon9" shape="poly" coords="742,104,465,361,425,471,444,741,224,667,184,308,345,273,642,68" href="lyon9.php" target="_blank" alt="lyonNeuf">
-        </map>"
-    </div>
+        <div class="map-container">
+            <canvas></canvas>
+            <img class="image" src='Images/map_globale_lyon.png' alt='lyonMap' usemap='#map'>
+           <map name='map'>          
+            <area shape="poly" coords="711,35,687,92,662,55,612,84,570,86,582,122,539,121,495,221,423,268,415,307,361,269,303,276,310,301,263,309,242,288,173,330,189,357,253,355,256,431,279,483,290,510,240,583,204,649,202,670,333,685,447,759,435,671,480,673,502,646,531,663,534,633,471,593,424,498,479,357,630,211,719,171,755,109,724,96" href='lyon9.php' target="_blank" alt="lyonNeuf">
+            <area target="" alt="" title="" href="" coords="526,302,567,333,618,392,719,409,770,453,736,536,633,528,469,570,430,485,484,357" shape="poly">    
+        </map>
+        </div>
     <main>
         <h2>Qui sommes-nous ?</h2>
         <p>Bla bla bla</p>
     </main>
 
     <?php include("_footer.php"); ?>
-
+    <script src="imagehighlighter.js"></script>
+<script src="index.js"></script>
 </body>
 
 </html>
